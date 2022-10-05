4422a288e90bc834ecf509d24b559893dd4850a4
elvisheddy <luongtuananh1405@gmail.com>
2022-10-05 22:37:00 +0700

    implement add enemy and checkCollision

--- main.js
+++ main.js
@@ -94,19 +94,68 @@ const loadCanvas = () => {
 
     shootTop() {
       if (this.game.ammo > 0) {
-        this.projectitles.push(new Projectitle(this.game, this.x, this.y));
+        this.projectitles.push(
+          new Projectitle(this.game, this.x + 80, this.y + 30)
+        );
         this.game.ammo--;
       }
     }
   }
 
-  class Enemy {}
+  class Enemy {
+    constructor(game) {
+      this.game = game;
+      this.x = this.game.width;
+      this.speedX = Math.random() * -1.5 - 0.5;
+      this.markedForDeletion = false;
+      this.lives = 5;
+      this.score = this.lives;
+    }
+
+    update() {
+      this.x += this.speedX;
+      if (this.x + this.width < 0) {
+        this.markedForDeletion = true;
+      }
+    }
+
+    draw(context) {
+      context.fillStyle = "red";
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.fillStyle = "white";
+      context.font = "20px Helvetic";
+      context.fillText(this.lives, this.x, this.y);
+    }
+  }
+
+  class Angeler1 extends Enemy {
+    constructor(game) {
+      super(game);
+      this.width = 228 * 0.2;
+      this.height = 169 * 0.2;
+      this.y = Math.random() * (this.game.height * 0.9 - this.height);
+    }
+  }
 
   class Layer {}
 
   class Background {}
 
-  class UI {}
+  class UI {
+    constructor(game) {
+      this.game = game;
+      this.fontSize = 25;
+      this.fontFamily = "Helvetica";
+      this.color = "yellow";
+    }
+
+    draw(context) {
+      context.fillStyle = this.color;
+      for (let i = 0; i < this.game.ammo; i++) {
+        context.fillRect(20 + 5 * i, 50, 3, 20);
+      }
+    }
+  }
 
   class Game {
     constructor(width, height) {
@@ -114,29 +163,91 @@ const loadCanvas = () => {
       this.height = height;
       this.player = new Player(this);
       this.input = new InputHandler(this);
+      this.ui = new UI(this);
       this.keys = [];
+      this.enemies = [];
       this.ammo = 20;
+      this.maxAmmo = 50;
+      this.ammoTimer = 0;
+      this.ammoInterval = 500;
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
+      this.gameOver = false;
     }
 
-    update() {
+    update(deltaTime) {
       this.player.update();
+      if (this.ammoTimer > this.ammoInterval) {
+        if (this.ammo < this.maxAmmo) {
+          this.ammo++;
+        } else {
+          this.ammoTimer = 0;
+        }
+      } else {
+        this.ammoTimer += deltaTime;
+      }
+
+      this.enemies.forEach((enemy) => {
+        enemy.update();
+        if (this.checkCollision(this.player, enemy)) {
+          enemy.markedForDeletion = true;
+        }
+        this.player.projectitles.forEach((projectitle) => {
+          if (this.checkCollision(projectitle, enemy)) {
+            enemy.lives--;
+            projectitle.markedForDeletion = true;
+            if (enemy.lives <= 0) {
+              enemy.markedForDeletion = true;
+              this.score += enemy.score;
+            }
+          }
+        });
+      });
+
+      this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
+
+      if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
     }
 
     draw(context) {
       this.player.draw(context);
+      this.ui.draw(context);
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+      });
+    }
+
+    addEnemy() {
+      this.enemies.push(new Angeler1(this));
+    }
+
+    checkCollision(rect1, rect2) {
+      return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.height + rect1.y > rect2.y
+      );
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-
-  const animate = () => {
+  let lastTime = 0;
+  const animate = (timeStamp) => {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.update();
+    game.update(deltaTime);
     game.draw(ctx);
     requestAnimationFrame(animate);
   };
 
-  animate();
+  animate(0);
 };
 
 window.addEventListener("load", loadCanvas);
